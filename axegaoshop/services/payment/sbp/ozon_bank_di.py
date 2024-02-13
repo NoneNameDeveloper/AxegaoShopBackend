6e17cfdb8bf7098ad625beb0f095db70ff08b97a
homy <user@gmail>
2024-02-13 22:52:10 +0300

    added raw function to check ozone bank auth data to valid

--- axegaoshop/services/payment/sbp/ozon_bank_di.py
+++ axegaoshop/services/payment/sbp/ozon_bank_di.py
@@ -3,7 +3,7 @@ from axegaoshop.services.payment.sbp.ozon_bank import OzoneBank
 
 
 async def get_ozone_bank() -> OzoneBank | None:
-    """получение объекта OzoneBank для работы с платегой"""
+    """получение объекта OzoneBank для работы с платегой из БД"""
     data = await get_ozone_bank_data()
 
     if not data:
@@ -14,3 +14,15 @@ async def get_ozone_bank() -> OzoneBank | None:
         secure_refresh_token=data.token
     ).prepare()
 
+
+async def get_ozone_bank_raw(token: str, pin_code: str) -> OzoneBank | None:
+    """получение объекта OzoneBank для работы с платегой"""
+    data = await get_ozone_bank_data()
+
+    if not data:
+        return None
+
+    return await OzoneBank(
+        pin_code=pin_code,
+        secure_refresh_token=token
+    ).prepare()
