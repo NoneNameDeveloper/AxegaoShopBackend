4c4b4ec69313fa1144275afc14dc6b5c0030e990
homy <user@gmail>
2024-03-13 16:30:56 +0300

    fix

--- axegaoshop/web/api/payment_settings/sbp/ozone_bank/views.py
+++ axegaoshop/web/api/payment_settings/sbp/ozone_bank/views.py
@@ -2,7 +2,8 @@ from fastapi import APIRouter, Depends, HTTPException
 
 from axegaoshop.db.models.payment_settings import PaymentSettingsOzone, get_ozone_bank_data
 from axegaoshop.services.payment.sbp.ozon_bank_di import get_ozone_bank_raw
-from axegaoshop.web.api.payment_settings.sbp.ozone_bank.schema import PaymentSettingsCreate, PaymentSettingsIn
+from axegaoshop.web.api.payment_settings.sbp.ozone_bank.schema import PaymentSettingsCreate, PaymentSettingsIn, \
+    PaymentSettingsUser
 
 from axegaoshop.services.security.jwt_auth_bearer import JWTBearer
 from axegaoshop.services.security.users import current_user_is_admin
@@ -31,7 +32,15 @@ async def create_payment_setting_ozone_bank(payment_settings: PaymentSettingsCre
     response_model=PaymentSettingsIn
 )
 async def get_payment_settings_ozone_bank():
-    """НЕ ИСПОЛЬЗУЕТСЯ"""
+    """получение данных по озон банку для админов"""
     return PaymentSettingsIn.model_validate(await get_ozone_bank_data())
 
 
+@router.get(
+    "/payments/sbp/ozon/user",
+    dependencies=[Depends(JWTBearer())],
+    response_model=PaymentSettingsUser
+)
+async def get_payment_settings_ozone_bank_user():
+    """получение данных по озон банку для пользователей"""
+    return PaymentSettingsUser.model_validate(await get_ozone_bank_data())
