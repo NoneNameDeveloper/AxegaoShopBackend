2c36002292de4ee805caac555343d635b66182ac
server <homy@1861669-cz55680.twc1.net>
2024-04-10 20:11:37 +0300

    s

--- axegaoshop/settings.py
+++ axegaoshop/settings.py
@@ -41,7 +41,7 @@ class Settings(BaseSettings):
 
     # конфигурация uvicorn
     scheme: str = "http"
-    host: str = "127.0.0.1"
+    host: str = "fileshare.su"
     port: int = 8000
 
     front_hostname: str = "http://fileshare.su:3000"
@@ -94,11 +94,12 @@ class Settings(BaseSettings):
     @property
     def base_hostname(self) -> URL:
         """создание хостнейма на сайт"""
-        return URL.build(
-            scheme=self.scheme,
-            host=self.host,
-            port=self.port
-        )
+        return "http://fileshare.su:8000"
+	#return URL.build(
+        #    scheme=self.scheme,
+        #    host=self.host,
+        #    port=self.port
+        #)
 
     @property
     def db_url(self) -> URL:
