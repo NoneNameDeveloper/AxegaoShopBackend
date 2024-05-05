203438510aca717e8b2b01dbb6f47677f9765c17
homy <user@gmail>
2024-05-05 22:32:44 +0300

    fixed pruning functional to free sums-to-pay

--- axegaoshop/services/crons/clear_database.py
+++ axegaoshop/services/crons/clear_database.py
@@ -3,9 +3,8 @@ from datetime import timedelta
 
 from axegaoshop.db.models.order import Order
 from axegaoshop.db.models.replenish import Replenish
-from axegaoshop.services.cache.redis_service import rem_amount
-
 from axegaoshop.logging import logger
+from axegaoshop.services.cache.redis_service import rem_amount
 
 
 async def clear_amount_of_purchasing() -> None:
@@ -20,7 +19,7 @@ async def clear_amount_of_purchasing() -> None:
     )
 
     await orders.update(status="canceled")
-    await orders.update(status="canceled")
+    await replenishes.update(status="canceled")
 
     order_amounts = await orders.values_list("result_price")
     replenish_amounts = await replenishes.values_list("result_price")
@@ -32,7 +31,7 @@ async def clear_amount_of_purchasing() -> None:
     for value in total:
         if len(value) != 0 and value[0]:
             pruned_count += 1
-            await rem_amount(float(value[0]))
+            await rem_amount(float(value[0] - int(value[0])))
 
     if pruned_count != 0:
-        logger.success(f"Pruned orders successfully: {pruned_count}")
+        logger.success(f"Pruned orders+replenishes successfully: {pruned_count}")
